d9aeabe68138b9adfe3a45cc5d665e7c2e8c7410
Douglas Morais <douglascarvalhomorais@hotmail.com>
2022-12-11 16:39:34 -0300

    feat(main): add cli first version with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Git cz worked!");
+    struct Cli {
+        pattern: String,
+        path: std::path::PathBuf,
+    }
+
+    let pattern = std::env::args().nth(0).expect("no pattern given");
+    let path = std::env::args().nth(1).expect("no path given");
+    let args = Cli {
+        path: std::path::PathBuf::from(path),
+        pattern: pattern,
+    };
 }
